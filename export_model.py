d775de7c531404e5ec7624c2fb6bd01b4df4724b
loguerci <lois.guerci78@gmail.com>
2026-05-15 02:52:06 +0000

    create model export script

---
+++ export_model.py
@@ -0,0 +1,26 @@
+import torch
+from torch.export import export
+import yaml
+from models.wavenext import WaveNeXt
+from utils.mel import MelSpectra
+
+def load_config(config_path):
+    with open(config_path, 'r') as f:
+        config = yaml.safe_load(f)
+    return config
+
+config = load_config('/home/lois/wavenext/config_24k.yaml')
+
+model = WaveNeXt(dim=config['dim'], 
+                 sample_rate=config['sample_rate'], 
+                 fft_dim=config['fft_dim'], 
+                 shift_dim=config['shift_dim'], 
+                 n_mels=config['n_mels'], 
+                 k=config['k'], 
+                 lr=config['learning_rate']).to('cuda')
+
+model.load_state_dict(torch.load("/home/lois/wavenext/checkpoints/07-05_at_03_56_57/wavenext-epoch=453-val_mel_loss=1.89.ckpt")['state_dict'])
+model.eval()
+
+export_model= torch.jit.script(model.generator)
+torch.jit.save(export_model, "my_wavenext_24k.pt")
